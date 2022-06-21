0c7900ae72875f140f128132aa79b36daac05c46
Sugi <sugidaffection@gmail.com>
2022-06-21 22:05:41 +0700

    update using piston_window

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,104 @@
-extern crate piston;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate glutin_window;
-
-use piston::window::WindowSettings;
-use piston::event_loop::{EventSettings, Events, EventLoop};
-use piston::input::{Button, Key, PressEvent, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use glutin_window::GlutinWindow;
-use opengl_graphics::{GlGraphics, OpenGL};
-
+use piston_window::*;
 mod snake;
-use snake::{Snake, Food};
+use snake::{Food, Snake};
 
 pub struct App {
-	gl: GlGraphics,
-	snake: Snake,
-	food: Food,
-	height: i32,
-	width: i32,
+    snake: Snake,
+    food: Food,
+    height: i32,
+    width: i32,
 }
 
 impl App {
+    /// Reset game state
+    fn reset(&mut self) {
+        self.snake = Snake::new(0, 0, 20, 1);
+        let mut food = Food::new(20);
+        food.spawn(400, 400);
+        self.food = food;
+    }
 
-	fn reset(&mut self){
-		self.snake = Snake::new(0, 0, 20, 1);
-		let mut food = Food::new(20);
-		food.spawn(400, 400);
-		self.food = food;
-	}
+    fn render<B: Graphics>(&mut self, t: math::Matrix2d, b: &mut B) {
+        const BACKGROUND: [f32; 4] = [0.3, 0.3, 0.3, 1.0];
 
-	fn render(&mut self, args: &RenderArgs){
-		use graphics::clear;
-		
-		const BACKGROUND: [f32; 4] = [0.3, 0.3, 0.3, 1.0];
+        clear(BACKGROUND, b);
 
-		self.width = args.width as i32;
-		self.height = args.height as i32;
+        self.food.render(t, b);
+        self.snake.render(t, b);
+    }
 
-		self.gl.draw(args.viewport(), |_, gl| {
-			clear(BACKGROUND, gl);
-		});
+    fn render_update(&mut self, args: &RenderArgs) {
+        // Update window size
+        self.width = args.window_size[0] as i32;
+        self.height = args.window_size[1] as i32;
+    }
 
-		self.food.render(&mut self.gl, args);
-		self.snake.render(&mut self.gl, args);
-	}
+    fn update(&mut self, _args: &UpdateArgs) {
+        self.snake.update();
+        let food = &self.food;
+        if self.snake.eat(food) {
+            self.food.spawn(self.width as i32, self.height as i32);
+        }
 
-	fn update(&mut self, _args: &UpdateArgs){
-		self.snake.update();
-		let food = &self.food;
-		if self.snake.eat(food){
-			self.food.spawn(self.width as i32, self.height as i32);
-		}
+        if self.snake.hit_screen(self.width, self.height) || self.snake.hit_body() {
+            self.reset();
+        }
+    }
 
-		if self.snake.hit_screen(self.width, self.height) ||
-			self.snake.hit_body(){
-			self.reset();
-		}
-	}
-
-	fn press(&mut self, args: &Button) {
-		if let &Button::Keyboard(key) = args {
-			match key {
-				Key::Up => {
-					self.snake.up();
-				}
-				Key::Down => {
-					self.snake.down();
-				}
-				Key::Left => {
-					self.snake.left();
-				}
-				Key::Right => {
-					self.snake.right();
-				}
-				_ => {}
-			}
-		}
-	}
+    fn press(&mut self, args: &Button) {
+        if let &Button::Keyboard(key) = args {
+            match key {
+                Key::Up => {
+                    self.snake.up();
+                }
+                Key::Down => {
+                    self.snake.down();
+                }
+                Key::Left => {
+                    self.snake.left();
+                }
+                Key::Right => {
+                    self.snake.right();
+                }
+                _ => {}
+            }
+        }
+    }
 }
 
 fn main() {
+    let mut window: PistonWindow = WindowSettings::new("Snake", (640, 480))
+        .exit_on_esc(true)
+        .build()
+        .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
 
-	let opengl = OpenGL::V4_1;
-	let mut window: GlutinWindow = WindowSettings::new("Snake", [400, 400])
-		.opengl(opengl)
-		.exit_on_esc(true)
-		.build()
-		.unwrap();
+    // update per seconds
+    window.set_ups(15);
 
-	let mut food = Food::new(20);
-	food.spawn(400, 400);
-	
-	let mut app = App {
-		gl: GlGraphics::new(opengl),
-		snake: Snake::new(0, 0, 20, 1),
-		food: food,
-		height: 400,
-		width: 400,
-	};
+    let mut food = Food::new(20);
+    food.spawn(400, 400);
 
-	let mut events = Events::new(EventSettings::new()).ups(15);
+    let mut app = App {
+        snake: Snake::new(0, 0, 20, 1),
+        food: food,
+        height: 400,
+        width: 400,
+    };
 
-	while let Some(e) = events.next(&mut window){
-		if let Some(r) = e.render_args() {
-			app.render(&r);
-		}
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _d| {
+            app.render(c.transform, g);
+        });
+        if let Some(r) = e.render_args() {
+            app.render_update(&r);
+        }
 
-		if let Some(u) = e.update_args() {
-			app.update(&u);
-		}
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
 
-		if let Some(b) = e.press_args() {
-			app.press(&b);
-		}
-	}
-
+        if let Some(b) = e.press_args() {
+            app.press(&b);
+        }
+    }
 }
--- src/snake.rs
+++ src/snake.rs
@@ -1,168 +1,173 @@
-use piston::input::{RenderArgs};
-use opengl_graphics::{GlGraphics};
-use graphics::{rectangle, Transformed};
+use crate::math::Matrix2d;
+use graphics::{ellipse, rectangle, DrawState, Graphics, Transformed};
 use rand::{thread_rng, Rng};
 
 #[derive(Clone)]
 struct Vec2 {
-	x: i32,
-	y: i32,
+    x: i32,
+    y: i32,
 }
 
 impl Vec2 {
+    fn collide(&mut self, target: &Vec2) -> bool {
+        self.x == target.x && self.y == target.y
+    }
 
-	fn collide(&mut self, target: &Vec2) -> bool{
-		self.x == target.x && self.y == target.y
-	}
+    fn generate_pos(&mut self, max: i32, step: i32) -> i32 {
+        let mut thread = thread_rng();
+        let n = thread.gen_range(0..max);
+        if n % step == 0 {
+            return n;
+        } else {
+            return self.generate_pos(max, step);
+        }
+    }
 
-	fn generate_pos(&mut self, max: i32, step: i32) -> i32{
-		let mut thread = thread_rng();
-		let n = thread.gen_range(0, max);
-		if n % step == 0 { 
-			return n;
-		}else{
-			return self.generate_pos(max, step);
-		}
-		
-	}
-
-	fn random_pos(&mut self, max_x:i32, max_y:i32, step:i32){
-		self.x = self.generate_pos(max_x, step);
-		self.y = self.generate_pos(max_y, step);
-	}
+    fn random_pos(&mut self, max_x: i32, max_y: i32, step: i32) {
+        self.x = self.generate_pos(max_x, step);
+        self.y = self.generate_pos(max_y, step);
+    }
 }
 
 pub struct Food {
-	pos: Vec2,
-	size: i32
+    pos: Vec2,
+    size: i32,
 }
 
-impl Food{
-	pub fn new(size: i32) -> Food{
-		let food = Food{
-			pos: Vec2{x:0, y:0},
-			size: size
-		};
+impl Food {
+    pub fn new(size: i32) -> Food {
+        let food = Food {
+            pos: Vec2 { x: 0, y: 0 },
+            size: size,
+        };
 
-		food
-	}
+        food
+    }
 
-	pub fn spawn(&mut self, w: i32, h: i32){
-		self.pos.random_pos(w, h, self.size);
-	}
+    /// Spawn food in random position inside screen
+    pub fn spawn(&mut self, w: i32, h: i32) {
+        self.pos.random_pos(w, h, self.size);
+    }
 
-	pub fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs){
-		const COLOR: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-		let rect = rectangle::square(0.0, 0.0, self.size as f64);
-		gl.draw(args.viewport(), |c, gl| {
-			rectangle(COLOR, rect, c.transform.trans(self.pos.x as f64, self.pos.y as f64), gl);
-		});
-		
-	}
+    pub fn render<B: Graphics>(&mut self, t: Matrix2d, b: &mut B) {
+        const COLOR: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        ellipse::Ellipse::new(COLOR).draw(
+            [0.0, 0.0, self.size as f64, self.size as f64],
+            &DrawState::default(),
+            t.trans(self.pos.x as f64, self.pos.y as f64),
+            b,
+        );
+    }
 }
 
 pub struct Snake {
-	head: Vec2,
-	body: Vec<Vec2>,
-	vel: Vec<i32>,
-	size: i32,
-	speed: i32,
-	dead: bool
+    head: Vec2,
+    body: Vec<Vec2>,
+    vel: Vec<i32>,
+    size: i32,
+    speed: i32,
+    dead: bool,
 }
 
 impl Snake {
+    pub fn new(x: i32, y: i32, size: i32, speed: i32) -> Snake {
+        Snake {
+            head: Vec2 { x, y },
+            body: vec![],
+            vel: vec![0, 0],
+            size: size,
+            speed: speed,
+            dead: false,
+        }
+    }
 
-	pub fn new(x: i32, y: i32, size: i32, speed: i32) -> Snake{
-		Snake{
-			head: Vec2{x, y},
-			body: vec![],
-			vel: vec![0,0],
-			size: size,
-			speed: speed,
-			dead: false
-		}
-	}
+    // Check if snake is hit screen border
+    pub fn hit_screen(&mut self, width: i32, height: i32) -> bool {
+        self.head.x < 0 || self.head.x > width || self.head.y < 0 || self.head.y > height
+    }
 
-	pub fn hit_screen(&mut self, width: i32, height: i32) -> bool{
-		self.head.x < 0 || self.head.x > width ||
-			self.head.y < 0 || self.head.y > height
-	}
+    /// Check if snake is colliding with bodies
+    pub fn hit_body(&mut self) -> bool {
+        for (i, body) in self.body.iter().enumerate() {
+            if i > 0 && self.head.collide(&body) {
+                return true;
+            }
+        }
 
-	pub fn hit_body(&mut self) -> bool{
-		for (i, body) in self.body.iter().enumerate() {
-			if i > 0 && self.head.collide(&body){
-				return true;
-			}
-		}
+        return false;
+    }
 
-		return false;
-	}
+    pub fn render<B: Graphics>(&mut self, t: Matrix2d, b: &mut B) {
+        const HEAD: [f32; 4] = [1.0, 0.8, 0.5, 1.0];
+        const BODY: [f32; 4] = [1.0, 0.5, 0.8, 1.0];
+        let rect = rectangle::square(0.0, 0.0, self.size as f64);
+        for body in self.body.iter() {
+            rectangle(BODY, rect, t.trans(body.x as f64, body.y as f64), b);
+        }
+        rectangle(
+            HEAD,
+            rect,
+            t.trans(self.head.x as f64, self.head.y as f64),
+            b,
+        );
+    }
 
-	pub fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs){
-		const HEAD: [f32; 4] = [1.0, 0.8, 0.5, 1.0];
-		const BODY: [f32; 4] = [1.0, 0.5, 0.8, 1.0];
-		let rect = rectangle::square(0.0, 0.0, self.size as f64);
-		gl.draw(args.viewport(), |c, gl| {
-			for body in self.body.iter(){
-				rectangle(BODY, rect, c.transform.trans(body.x as f64, body.y as f64), gl);
-			}
-			rectangle(HEAD, rect, c.transform.trans(self.head.x as f64, self.head.y as f64), gl);
+    pub fn eat(&mut self, food: &Food) -> bool {
+        let pos = &food.pos;
+        // check if snake head is colliding with food
+        if self.head.collide(pos) {
+            // create new tail from head position if body is empty
+            // else create from current tail position
+            self.body.push(if self.body.len() > 0 {
+                self.body.last().unwrap().clone()
+            } else {
+                self.head.clone()
+            });
 
-		});
-		
-	}
+            return true;
+        }
 
-	pub fn eat(&mut self, food:&Food) -> bool{
-		let pos = &food.pos;
-		if self.head.collide(pos){
+        false
+    }
 
-			self.body.push(if self.body.len() > 0 { self.body.last().unwrap().clone() } else { self.head.clone()});
-			
-			return true
-		}
+    pub fn update(&mut self) {
+        // check if snake alive
+        if !self.dead {
+            // insert new body from current head position
+            self.body.insert(0, self.head.clone());
+            // update head position
+            self.head.x += self.vel[0] * self.size;
+            self.head.y += self.vel[1] * self.size;
+            // remove tail
+            self.body.pop();
+        }
+    }
 
-		false
-	}
+    pub fn up(&mut self) {
+        if self.vel[1] == 0 {
+            self.vel[0] = 0;
+            self.vel[1] = -self.speed;
+        }
+    }
 
-	pub fn update(&mut self) {
-		if !self.dead {
-			self.body.insert(0, self.head.clone());
-			self.head.x += self.vel[0] * self.size;
-			self.head.y += self.vel[1] * self.size;
-			self.body.pop();
-		}
-		
-	}
+    pub fn down(&mut self) {
+        if self.vel[1] == 0 {
+            self.vel[0] = 0;
+            self.vel[1] = self.speed;
+        }
+    }
 
-	pub fn up(&mut self) {
-		if self.vel[1] == 0{
-			self.vel[0] = 0;
-			self.vel[1] = -self.speed;
-		}
-		
-	}
+    pub fn left(&mut self) {
+        if self.vel[0] == 0 {
+            self.vel[0] = -self.speed;
+            self.vel[1] = 0;
+        }
+    }
 
-	pub fn down(&mut self){
-		if self.vel[1] == 0{
-			self.vel[0] = 0;
-			self.vel[1] = self.speed;
-		}
-	}
-
-	pub fn left(&mut self){
-		if self.vel[0] == 0{
-			self.vel[0] = -self.speed;
-			self.vel[1] = 0;
-		}
-		
-	}
-
-	pub fn right(&mut self){
-		if self.vel[0] == 0{
-			self.vel[0] = self.speed;
-			self.vel[1] = 0;
-		}
-		
-	}
-
+    pub fn right(&mut self) {
+        if self.vel[0] == 0 {
+            self.vel[0] = self.speed;
+            self.vel[1] = 0;
+        }
+    }
 }
